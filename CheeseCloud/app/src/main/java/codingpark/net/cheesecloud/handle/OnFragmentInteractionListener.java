982f754d6202cf59dd21956c6ef838d7c7b5050b
fangjingfeng <123@computer>
2015-05-11 17:50:23 +0800

    2015年5月11号星期一17:50

--- CheeseCloud/app/src/main/java/codingpark/net/cheesecloud/handle/OnFragmentInteractionListener.java
+++
@@ -1,9 +0,0 @@
-package codingpark.net.cheesecloud.handle;
-
-/**
- * Created by ethanshan on 14-9-19.
- * For Fragment interact with its host Activity
- */
-public interface OnFragmentInteractionListener {
-    public void onFragmentInteraction(String id);
-}
--- CheeseCloud/app/src/main/java/codingpark/net/cheesecloud/handle/OnSettingListener.java
+++
@@ -1,9 +0,0 @@
-package codingpark.net.cheesecloud.handle;
-
-/**
- * Created by ethanshan on 14-11-27.
- * For FragmentSetting interact with MainActivity
- */
-public interface OnSettingListener {
-    public void logout();
-}
